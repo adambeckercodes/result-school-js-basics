5b794823e385df11197a7734a06b4c2c1e7199a4
adambecker <adambeckercodes@gmail.com>
2024-01-08 10:08:37 +0600

    update homework

---
+++ homework/main.js
@@ -0,0 +1,81 @@
+// №1
+
+// var a = 5;
+// let b = 10;
+// const c = 13;
+
+// console.log(a, b, c);
+
+// a = 15;
+// b = 34;
+// // c = 54; // Нельзя давайть новое значение константам
+
+// console.log(a, b, c);
+
+// №2
+
+// const a = 100;
+// // a = 20; // Нельзя переназначать константу
+// console.log(a);
+
+// // console.log(a); // Переменная еще не инициализирована
+// let a = 15;
+// console.log(a);
+
+// console.log(a);
+// var a;
+// a = 10;
+// console.log(a);
+
+// №3
+// let productName = "Вода";
+// let productPrice = 150;
+// let productQuantity = 20;
+// let resultCost = productPrice * productQuantity;
+
+// console.log(`Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} рублей. Всего было потрачено: ${resultCost} рублей.`);
+
+// №4
+// console.log(typeof 'Hello'); // String
+// console.log(typeof 10); // Number
+// console.log(typeof {}); // Object
+// console.log(typeof null); // Object
+// console.log(undefined); // undefined
+// console.log(typeof true); // boolean
+// console.log(typeof []) // object
+// console.log(typeof (() => {})); // function
+
+//№5
+// console.log(10 == 10); // true
+// console.log(10 != 10); // false
+// console.log(12 == 'Hello'); // false
+// console.log(10 < 11); // true
+// console.log(12 > 20); // false
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false || true); // true
+// console.log(true || true); // true
+// console.log(10 <= 10); // true
+// console.log(18 <= 20); // true
+// console.log(50 >= 50); // true
+// console.log(51 >= 50); // true
+// console.log({} === {}); // false
+// console.log({} == {}); // false
+
+// const a = {};
+// const b = a;
+// console.log(a === b); // true
+
+
+// №6
+
+// let a = 10;
+// let b = 23;
+
+// console.log(a > b);
+
+// №7
+// let a = 5;
+// let b = 7;
+
+// let c = (a > b) ? a : b;
