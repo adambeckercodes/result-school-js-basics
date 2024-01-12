614a0f1dd881cb1e8bcdd06e10e79e2e348f0c1c
adambecker <adambeckercodes@gmail.com>
2024-01-12 17:33:33 +0600

    update

--- 02-base/app.js
+++ 02-base/app.js
@@ -17,46 +17,47 @@ const inputElement = document.getElementById("title");
 const createBtn = document.getElementById("create");
 const listElement = document.getElementById("list");
 
-const notes = ["write a blog post about js arrays", "explain array theory"];
+// const notes = ["write a blog post about js arrays", "explain array theory"];
 
-function render() {
-  // for (let i = 0; i < notes.length; i++) {
-  //   listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]));
-  // }
+// function render() {
+//   // for (let i = 0; i < notes.length; i++) {
+//   //   listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]));
+//   // }
 
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
-  }
-}
+//   for (let note of notes) {
+//     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+//   }
+// }
 
-render();
+// render();
 
-createBtn.onclick = function () {
-  // listElement.innerHTML = ;
-  if (inputElement.value.length === 0) {
-    return;
-  }
+// createBtn.onclick = function () {
+//   // listElement.innerHTML = ;
+//   if (inputElement.value.length === 0) {
+//     return;
+//   }
 
-  listElement.insertAdjacentHTML(
-    "beforeend",
-    getNoteTemplate(inputElement.value)
-  );
+//   listElement.insertAdjacentHTML(
+//     "beforeend",
+//     getNoteTemplate(inputElement.value)
+//   );
 
-  inputElement.value = ``;
-};
+//   inputElement.value = ``;
+// };
 
-function getNoteTemplate(title) {
-  return `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${title}</span>
-      <span>
-        <span class="btn btn-small btn-success">&check;</span>
-        <span class="btn btn-small btn-danger">&times;</span>
-      </span>
-    </li>`;
-}
+// function getNoteTemplate(title) {
+//   return `
+//     <li class="list-group-item d-flex justify-content-between align-items-center">
+//       <span>${title}</span>
+//       <span>
+//         <span class="btn btn-small btn-success">&check;</span>
+//         <span class="btn btn-small btn-danger">&times;</span>
+//       </span>
+//     </li>`;
+// }
 
-/* Objects Theory */
+/*
+Objects Theory
 const person = {
   firstName: "Dias",
   lastName: "Adambek",
@@ -67,3 +68,55 @@ const person = {
     console.log(person.firstName + ' ' + person.lastName)
   },
 };
+
+person.getFullName();
+*/
+
+const notes = [
+  {
+    title: "write a blog post about js arrays",
+    completed: false,
+  },
+  {
+    title: "explain array theory",
+    completed: true,
+  },
+];
+
+function render() {
+  for (let note of notes) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i));
+  }
+}
+render();
+
+createBtn.onclick = function () {
+  // listElement.innerHTML = ;
+  if (inputElement.value.length === 0) {
+    return;
+  }
+
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(newNote));
+
+  inputElement.value = ``;
+};
+
+function getNoteTemplate(note, index) {
+  return `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span class="${note.completed ? "text-decoration-line-through" : ""}">${
+    note.title
+  }</span>
+      <span>
+        <span class="btn btn-small btn-${
+          note.completed ? "warning" : "success"
+        }" data-index=${index}>&check;</span>
+        <span class="btn btn-small btn-danger">&times;</span>
+      </span>
+    </li>`;
+}
+
